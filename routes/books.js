a83ac6fd1d9fc1c1149e056907f373828c5e4e8a
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-05-31 21:21:40 -0500

    books listing

--- routes/books.js
+++ routes/books.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render("books/index")
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  const books = await Book.findAll();
+  res.render("books/index", { books })
+}));
 
 /* GET new book form. */
 router.get('/new', (req, res, next) => {
-  res.render("books/new-book")
+  res.render("books/new-book");
 });
 
 /* POST new book form. */
@@ -19,6 +33,16 @@ router.post('/new', (req, res, next) => {
 /* Book detail page. */
 router.get('/:id', (req, res, next) => {
   res.render("books/update-book")
+});
+
+/* POST update book details. */
+router.post('/:id', (req, res, next) => {
+  res.redirect("/books")
+});
+
+/* POST delete book. */
+router.post('/:id/delete', (req, res, next) => {
+  res.redirect("/books")
 });
 
 
